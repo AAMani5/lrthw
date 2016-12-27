96d734cc7e8a8d8f654697112dff784036016fd1
Ashwini <ashie.oh@gmail.com>
2016-12-27 11:56:54 +0000

    adding chapter 01

---
+++ chap01/ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather 'not'."
+puts 'I "said" do not touch this.'
+puts "make script print another line"
+# this a to comment a line
