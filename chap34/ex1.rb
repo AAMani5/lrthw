73edd13e7a01eef4550ae38100fb6a44591d4f16
Ashwini <ashie.oh@gmail.com>
2017-01-01 13:46:36 +0000

    adding chap34 exercises

---
+++ chap34/ex1.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+p animals[1] # animal at 1
+p animals[2] # 3rd animal
+p animals[0] # first animal
+p animals[3] # animal at 3
+p animals[4] # fifth animal
+p animals[2] # animal at 2
+p animals[5] # sixth animal
+p animals[4] # animal at 4
