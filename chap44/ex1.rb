dcc2403df3b3085ddbd8d685b533794b5f27834e
Ashwini <ashie.oh@gmail.com>
2017-01-17 10:33:21 +0000

    inheritance - implicit, override & alter

---
+++ chap44/ex1.rb
@@ -0,0 +1,43 @@
+# demonstrates 3 kinds of interaction - implicit, override & alter - from inheritance
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+
+  def altered()
+    puts "PARENT altered()"
+  end
+end
+
+class Child < Parent  # Child class inherits from parent class
+
+  def override()
+    puts "CHILD override()"  # overrides the method with same name in base or parent class
+  end
+
+  # the implicit function from base class is untouched so its not altered or overriden
+
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"  # change to altered() before calling the base class instance method with same name
+    super()  # base method with name name is call here
+    puts "CHILD, AFTER PARENT altered()"   # change to altered() after calling the base class instance method with same name
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
